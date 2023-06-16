2c5e4831babe81c474f19065fd982af99bce95b0
Exoutia <jhav365@gmail.com>
2023-06-16 16:51:31 +0530

    designing the basic components for room

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Grid, Button, Typography, Container } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class Room extends Component {
   constructor(props) {
@@ -28,13 +30,46 @@ export default class Room extends Component {
 
   render() {
     return (
-      <div>
+      <Grid container spacing={1} align="center">
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code : {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest Can Skip: {this.state.guestCanSkip.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Votes: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            You are Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+/*     <div>
         <h3>Code: {this.roomCode}</h3>
         <p>Votes: {this.state.votesToSkip}</p>
         <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
         <p>Guest Can Skip: {this.state.guestCanSkip.toString()}</p>
         <p>Host: {this.state.isHost.toString()}</p>
       </div>
-    );
-  }
-}
+*/
